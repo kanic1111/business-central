a6ccb356fae6b856132819ecf17ce81d383bed7f
kanic1111 <59955075+kanic1111@users.noreply.github.com>
2022-06-30 19:38:10 +0800

    Update build.sh
    change podman to docker

--- showcase/build.sh
+++ showcase/build.sh
@@ -11,6 +11,6 @@ IMAGE_TAG="7.71.0.Final"
 # Build the container image.
 echo "Building container for $IMAGE_NAME:$IMAGE_TAG.."
 # In case you want to build with Docker please use `docker` instead of ´podman´
-podman build --rm -t $IMAGE_NAME:$IMAGE_TAG .
+sudo docker build --rm -t $IMAGE_NAME:$IMAGE_TAG .
 echo "Build done"
 
